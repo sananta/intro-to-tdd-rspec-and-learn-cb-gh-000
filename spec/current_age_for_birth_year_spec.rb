478c2b97cf8725e9a8d626cbf2c8ccf6f7a1d31a
Varshini Ananta <sananta@ucdavis.edu>
2019-05-18 01:23:10 +0000

    Done.

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -4,6 +4,17 @@ describe "current_age_for_birth_year method" do
   it "returns the age of a person based on the year of birth" do
     age_of_person = current_age_for_birth_year(1984)
 
-    expect(age_of_person).to eq(19)
+    expect(age_of_person).to eq(Time.now.year-1984)
+  end
+end
+
+describe "current_age_for_birth_year method" do
+  it "returns the age of a person based on the year of birth" do
+    current_year = Time.now.year
+    birth_year = 1984
+    answer = current_year - birth_year
+
+    age_of_person = current_age_for_birth_year(birth_year)
+    expect(age_of_person).to eq(answer)
   end
 end
